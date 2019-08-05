843f3045241a45cd6bc2dfe5709f7a57a29c9748
beazlr02 <ross.beazley@bbc.co.uk>
2019-08-05 09:39:32 +0100

    First commit

---
+++ JustSomeAVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustSomeAVPlayer
+//
+//  Created by Ross Beazley on 05/08/2019.
+//  Copyright © 2019 Ross Beazley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JustSomeAVPlayerTests/JustSomeAVPlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  JustSomeAVPlayerTests.swift
+//  JustSomeAVPlayerTests
+//
+//  Created by Ross Beazley on 05/08/2019.
+//  Copyright © 2019 Ross Beazley. All rights reserved.
+//
+
+import XCTest
+@testable import JustSomeAVPlayer
+
+class JustSomeAVPlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
